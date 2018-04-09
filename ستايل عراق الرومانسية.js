4e42f729f0e5313bcaf3606cfa728926413590d6
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-09 16:38:52 +0300

    Update ستايل عراق الرومانسية.js

--- ستايل عراق الرومانسية.js
+++ ستايل عراق الرومانسية.js
@@ -1,5 +1,5 @@
 $(` <center><div></div><center>
-<div style=" width: 100%; "  id="as"collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">موسيقى</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i>
+<div style=" width: 100%; "  id="as" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">موسيقى</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i>
 <marquee direction="right" width="99%" id="asim0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >  &nbsp; قوانين الشات ● · في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● · وشكرآ لكم &nbsp;</marquee></font>
 </p>شغل الموسيقى حتى ليفصل الشات</p>
 </p>بدون صوت</p><audio controls loop="" autoplay="">
@@ -8,7 +8,7 @@ $(` <center><div></div><center>
 </div><center>
 `).insertBefore('div#d2bc');
 $(`<img src ="sico/z1c7j29n8e10.gif" etclass="fr" style="width:100%;margin-top: 1px;">
-<center><div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >  &nbsp; اتبع قول نبينا ﷺ اتق الله حيثما كنت  ●· لا تكسبوا رضا الناس اكسبوا رضا الله ● · لا تدع الدردشة تلهيك عن صلاتك ولا تستخدم الشات في ما لا يرضي الله &nbsp;</marquee></font></div><center></div>
+</div>
 <div id="asim10"><center><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
 <a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> Smoke <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @MBC04 </p> Asim <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @o7700000 </p> Dhargam <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @D1997IQ </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs');
 $(".checkbox label").html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`);
