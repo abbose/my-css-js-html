3ecf3d85bfffc54f54124df5bc33e8c5fb7f0302
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-09 22:04:13 +0300

    Update ستايل عراق الرومانسية.js

--- ستايل عراق الرومانسية.js
+++ ستايل عراق الرومانسية.js
@@ -1,6 +1,5 @@
 $(` <center><div></div><center>
 <div style=" width: 100%; "  id="as" margin: 2px;" class="fl mini fa fa-music btn btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">● · مـوسـيـقـى · ●</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i>
-<marquee direction="right" width="99%" id="asim0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >  &nbsp; قوانين الشات ● · في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● · وشكرآ لكم &nbsp;</marquee></font>
 </p>شغل الموسيقى حتى ليفصل الشات</p>
 </p>بدون صوت</p><audio controls loop="" autoplay="">
 <source src="https://iraqiachats.com/asim.mp3">
