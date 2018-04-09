d108e4c039575bbdc914b24f50455be118d9c926
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-10 02:50:25 +0300

    Add files via upload

---
+++ compressed.js
@@ -0,0 +1,121 @@
+'https:'!=location.protocol&&(location.protocol='https:'),$(`<img src="sultan.png" class="fl" style="width:48%;margin-right: 2px;">
+<a href="call.html" style="width:90px;margin:2px;" class="fl mini  fa   fa-phone btn btn-warning"> اتصل بنا</a>
+<a href="https://play.google.com/store/apps/details?id=com.w_6326306" style="width:85px;margin:2px;" class="fl mini  fa  fa-heartbeat
+ btn btn-danger"> التطبيـق</a>
+<a href="sub.html" style="width:87px;margin:2px;" class="fl mini  fa   fa-star
+ btn btn-success"> للإشتراك</a>
+<a href="rulles.html" style="width:90px;margin:2px;" class="fl mini  fa fa-graduation-cap
+btn btn-info"> القوانين</a>
+
+`).insertBefore('.nav-tabs'),$('.label-primary.mini.fl').css('border-radius','15px 15px 0px 0px'),$('.grow.break.tab-pane.fade.active.in').css({'background-color':'#F7F7F7'}),$('.border.corner.light.fr.break').css({'background-color':'#F7F7F7'}),$('.nav-tabs').addClass('fl').css('width','100%'),$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" type="text/css" href="skr.css">
+
+<style>
+.fl.mini.u-co {
+    display: none;
+}
+img.fr.co {
+    width: 0px!important;
+}
+.fl.mini.u-co img.fl {
+    width: 0px!important;
+}
+
+</style>`).insertBefore('body'),$('button.btn.btn-primary').css('border-radius','15px 15px 15px 15px'),$('.label-primary').css('border-radius','15px 15px 15px 15px'),$('.break.light').css({'background-image':'url(https://skr0.com/jawalskr0.jpg)'}),$('.nav-tabs').addClass('fl').css('width','100%'),$(` <center><div><marquee direction="right" width="99%" id="sultan0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >  &nbsp; قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم &nbsp;</marquee></font></div><center>
+`).insertBefore('div#d2bc'),$('.fa.fa-sign-out.fl.btn.btn-primary').hide(),$(`
+
+
+<div  class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div style="max-width: 390px;"class="modal-dialog" role="document">
+    <div class="modal-content"style="background-color: #FEF5FF;">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 style="text-align: center;font-family: 'DroidArabicKufiRegular',FontAwesome;"class="btn btn-primary btn-sultan" id="skr0-com"> قوانين الشات </h4>
+      </div>
+      <div style="padding: 0;" class="skrr">
+<div dir="rtl" style="text-align: right;padding: 0;margin-right: -20px;" trbidi="on">
+<ul class="main">
+      <li>
+        <div>
+          <span class="skrr"> تنبيه عند ذكر المواقع </span>
+          <br />
+          <span class="sultan-skr3">
+            ممنوع آي شخص يذكر آسم موقع اخر , وفي حال ذكر ذلك يتم حذف عضويته وحظره 
+         </span> <span style="color: red;">
+      </span>
+        </div>
+      </li>
+       
+       <li>
+        <div>
+          <span class="skrr"> تنبية للمراقبين جميعا </span>
+          <br />
+          <span class="sultan-skr3">يمنع طرد السوابر والإدارات لبعضهم لبعض هذا الامر يلزم الإدارة القيام به فقط  
+                   </span> <span style="color: red;">
+
+        </div>
+      </li>
+      
+       <li>
+        <div>
+          <span class="skrr"> عند وجود مضايقات </span>
+          <br />
+          <span class="sultan-skr3"> ممنوع أي شخص يغلط على شخص اخر والي يغلط يرجى الآثبات , واعطائه للمراقب او الإدارة  
+		  </span> <span style="color: red;">
+		
+</span>
+        </div>
+      </li>
+      
+       <li>
+        <div>
+          <span class="skrr">قانون الزحف </span>
+          <br />
+          <span class="sultan-skr3"> ممنوع اي شخص يضايق شخص ثاني بالعام او بالخاص او تنبيهات او جدار والي يسوي هيج ينطرد
+		  		  </span> <span style="color: red;">
+        </span>
+        </div>
+      </li>
+      
+       <li>
+        <div>
+          <span class="skrr"> قانون الايحاءات</span>
+          <br />
+          <span class="sultan-skr3">ممنوع التحدث بكلام غير اخلاقي حتى ولو كان غير مباشر
+		  		  		  </span> <span style="color: red;">
+
+		  </span>
+        </div>
+      </li>
+	         <li>
+        <div>
+          <span class="skrr"> قانون السياسة والدين</span>
+          <br />
+          <span class="sultan-skr3">ممنوع التحدث بالسياسه والدين ومن يخالف يتم حظره
+		  		  		  </span> <span style="color: red;">
+		  </span>
+        </div>
+      </li>
+	         <li>
+        <div>
+          <span class="skrr"> إلتنبيه الحائط للآبداع</span>
+          <br />
+          <span class="sultan-skr3">للنشر الحكم - والأمثال والخواطر الإبيات الشعريه متقطفات دينيه وليس للسوالف والدردشه للعام مفتوح للجميع
+		  		  		  </span> <span style="color: red;">
+
+</span>
+        </div>
+      </li>
+
+    </div>  
+    
+  
+</div>      </div>
+      <div class="modal-footer">
+        <button style="margin-right: 107px;" type="button" class="btn btn-danger" data-dismiss="modal">إغــلاق نـآفذة القوانين</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div 
+style="padding: 3px 2px 1px 2px;margin-right:4px;width: 100px;float: right;text-align: center;margin-top: -16px;border-radius: 15px;color: white;background-color: black;"  id="ass" class="btn btn-primary btn-asim" type="button"  data-toggle="modal" data-target="#myModal" > قوانين الشات</div>`).insertBefore('label.fl.nosel.label.pnhead');
