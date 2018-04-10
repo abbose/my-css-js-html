079b286d952815bd8c9d01cb789496fd57dbbb89
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-10 04:26:23 +0300

    Update ستايل عراق الرومانسية.js

--- ستايل عراق الرومانسية.js
+++ ستايل عراق الرومانسية.js
@@ -1,5 +1,4 @@
 $(` 
-
 <div class="uzr fl corner borderg mm bid1camh6so110" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
       <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(&quot;pic/1cak7fqj310.png.jpg&quot;);" class="fl fitimg hand u-pic    ">
       <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523322166256">الآن</span>
@@ -14,13 +13,8 @@ $(`
 &nbsp;<br />
 &nbsp;</div>
       </div>
-    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-primary fa fa-spin fa-heart fr"></a></div></p>
-
-
-</p><center><div></div><center>
-<div style=" width: 100%; "  id="as" margin: 2px;" class="btn btn-primary btn-asim" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">● · مـوسـيـقـى · ●</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i>
-</p>شغل الموسيقى حتى ليفصل الشات</p>
-</p>بدون صوت</p><audio controls loop="" autoplay="">
+    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-primary fa fa-spin fa-heart fr"></a></div>
+</p><center></p><audio controls loop="" autoplay="">
 <source src="https://iraqiachats.com/asim.mp3">
 </audio>
 </div><center>
