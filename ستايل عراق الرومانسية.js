501617ec6887a682562657cf102a700c7f7993ec
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-11 02:16:46 +0300

    Update ستايل عراق الرومانسية.js

--- ستايل عراق الرومانسية.js
+++ ستايل عراق الرومانسية.js
@@ -25,31 +25,14 @@ $(`<div class="uzr fl corner borderg mm bid1camh6so110" style="border-radius:5px
     <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="sico/z1c7j29n8e10.gif" etclass="fr" style="width:100%;margin-top: 1px;">
 </div>
 <div id="asim10"><center><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
-<a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> Smoke <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @MBC04 </p> Asim <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @o7700000 </p> Dhargam <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @D1997IQ </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$('<img src="sico/z1c7e1gv2n10.gif" style="width:100px;margin-top:-65px;" class="fr">').insertBefore('.loginstat'),$('.tc span[class="fa fl"]').addClass('fa-check'),nopm=!0,$(`<link rel="stylesheet" type="text/css" href="https://iqr30.github.io/my-css-js-html/شات عراق الرومانسية للجوال.css"><?php
-session_start(); 
-$_SESSION['css_show_key'] = "Mr.Ajax"; 
-?>
-<link rel="stylesheet" type="text/css" href="https://iqr30.github.io/my-css-js-html/شات عراق الرومانسية للجوال.css"><style>
+<a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> Smoke <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @MBC04 </p> Asim <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @o7700000 </p> Dhargam <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @D1997IQ </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$('<img src="sico/z1c7e1gv2n10.gif" style="width:100px;margin-top:-65px;" class="fr">').insertBefore('.loginstat'),$('.tc span[class="fa fl"]').addClass('fa-check'),nopm=!0,
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="https://iqr30.github.io/my-css-js-html/شات عراق الرومانسية للجوال.css">
+<style>
+.
+</style>`).insertBefore('body');
 
-
-
-#ass {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; background-color: #E80883; height: 20px; color: #FFFFFA; border-radius: 40px 40px 0px 40px!important; padding-bottom: 2px!important;
-    text-shadow: 2px 0px 8px rgba(255,255,219) , 0px 2px 10px #fff !important;
-border: 1px solid #FFF0FA
-}
-#asim0 {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; background-color: #780444; height: 25px; color: #FFFFFA; border-radius: 5px 5px 5px 5px; border-bottom: 2px solid #FF0897; border-top: 2px solid #FF0897; padding-bottom: 2px!important;
-    text-shadow: 2px 0px 8px rgba(255,255,219) , 0px 2px 10px #fff !important;
-
-}
-#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #FFFFFA; border-radius: 5px 5px 5px 5px;
-    text-shadow: 2px 0px 8px rgba(255,255,219) , 0px 2px 10px #fff !important;
-border: 1px solid #FFE0F6;
-height: 33px;
-text-align: center;
-}
-.btn-asim {background-image: url(https://iqr30.com/sico/z1c77cfdeg10.gif);}
-</style>
-`).insertBefore('head title'),$(`<center><div><marquee direction="right" width="99%" id="asim8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font color="#595757" class="asim12">عراق الرومانسية</font> ● · افضل سوبر لهذا الاسبوع  · ●<font class="asim11" color="#727A54"> اوديــت </font> ● · افضل زائر لهذا الاسبوع  · ●<font class="asim11" color="#ff9900"> ضــآيـج </font> ● · افضل زائرة لهذا الاسبوع  · ●<font class="asim11" color="#ff00ff"> آمـي  عـآفـيـتـي </font> ● · مبدع الحائط لهذا الاسبوع · ●<font class="asim11" color="#5CAD9A"> حـطـمـوك يـآقـلـبـي </font> ● · مبدعة الحائط لهذا الاسبوع · ●<font class="asim11" color="#800000"> نـتـالـي </font> ● · افضل تواجد لهذا الاسبوع · ●<font class="asim11" color="#417570"> رهـف بـنـت الـبـاشـا </font>  ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),document.getElementById('tbox').placeholder='\u0639\u0645\u064A \u0639\u0648\u0641 \u0627\u0644\u0633\u0637\u062D \u0648\u062F\u0631\u062F\u0634 \u0648\u064A\u0627\u0646\u0627',$('[data-target=#wall]').attr('title','\u0644\u062A\u0646\u0634\u0631\u0648\u0646 \u0627\u063A\u0627\u0646\u064A'),$(`
+$(`<center><div><marquee direction="right" width="99%" id="asim8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font color="#595757" class="asim12">عراق الرومانسية</font> ● · افضل سوبر لهذا الاسبوع  · ●<font class="asim11" color="#727A54"> اوديــت </font> ● · افضل زائر لهذا الاسبوع  · ●<font class="asim11" color="#ff9900"> ضــآيـج </font> ● · افضل زائرة لهذا الاسبوع  · ●<font class="asim11" color="#ff00ff"> آمـي  عـآفـيـتـي </font> ● · مبدع الحائط لهذا الاسبوع · ●<font class="asim11" color="#5CAD9A"> حـطـمـوك يـآقـلـبـي </font> ● · مبدعة الحائط لهذا الاسبوع · ●<font class="asim11" color="#800000"> نـتـالـي </font> ● · افضل تواجد لهذا الاسبوع · ●<font class="asim11" color="#417570"> رهـف بـنـت الـبـاشـا </font>  ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),document.getElementById('tbox').placeholder='\u0639\u0645\u064A \u0639\u0648\u0641 \u0627\u0644\u0633\u0637\u062D \u0648\u062F\u0631\u062F\u0634 \u0648\u064A\u0627\u0646\u0627',$('[data-target=#wall]').attr('title','\u0644\u062A\u0646\u0634\u0631\u0648\u0646 \u0627\u063A\u0627\u0646\u064A'),$(`
 
 
 <div  class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
