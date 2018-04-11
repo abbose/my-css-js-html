5a28c7b51f2137919742f53e8105fab36b48a2be
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-11 06:41:21 +0300

    Delete ستايل عراق الرومانسية.js

--- ستايل عراق الرومانسية.js
+++
@@ -1,138 +0,0 @@
-$(` <center><div></div><center>
-<div style=" width: 100%; "  id="as" margin: 2px;" class="btn btn-primary btn-asim" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">● · مـوسـيـقـى · ●</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i>
-</p>شغل الموسيقى حتى ليفصل الشات</p>
-<audio controls loop="" autoplay="">
-<source src="https://iraqiachats.com/asim.mp3">
-</audio>
-</div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
-
-
-
-$(`<div class="uzr fl corner borderg mm bid1camh6so110" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
-      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(&quot;pic/1cak7fqj310.png.jpg&quot;);" class="fl fitimg hand u-pic    ">
-      <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523322166256">الآن</span>
-      <div class="uzr fl" style="padding:0px;width:80%">
-        <div style="padding:0px;width:100%;" class="fl">
-          <img class="fl u-ico" alt="" src="sico/z1ca7mtgpj10.gif">
-          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(250, 250, 250); background-color: rgb(130, 130, 130);" class="corner nosel u-topic dots fl hand">  ❰أإسہٰٰ۫ـيآدهہٰٰ۫ــٰཻ℘ـہٰٰ۫ہٰٰ۫ـٰٰ۫م❱ ●・💀</span>
-        </div>
-        <br>
-        <div style="padding:0px; width:100%;" class=" u-msg   break  fl">فقد الكون امام وله تبكي السماء وعليه العين تدمع بالنياح والبكاء نلطم الصدر بحزنا ونجدد للــــولاء .. نعزي الأمة الأسلامية وصاحب العصر والزمان الإمام المهدي عجل الله فرجه الشريف بأستشهاد جده الأمام الکاظم عليه السلام.&nbsp;</div>
-      </div>
-&nbsp;<br />
-&nbsp;</div>
-      </div>
-    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="sico/z1c7j29n8e10.gif" etclass="fr" style="width:100%;margin-top: 1px;">
-</div>
-<div id="asim10"><center><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
-<a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> Smoke <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @MBC04 </p> Asim <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @o7700000 </p> Dhargam <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> @D1997IQ </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$('.tc span[class="fa fl"]').addClass('fa-check'),nopm=!0,$(`<link rel="stylesheet" type="text/css" href="https://iqr30.github.io/my-css-js-html/iiqr30.css"><?php
-session_start(); 
-$_SESSION['css_show_key'] = "Mr.Ajax"; 
-?>
-<link rel="stylesheet" type="text/css" href=""><style>
-</style>
-`).insertBefore('head title'),$(`<center><div><marquee direction="right" width="99%" id="asim8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font color="#595757" class="asim12">عراق الرومانسية</font> ● · افضل سوبر لهذا الاسبوع  · ●<font class="asim11" color="#727A54"> اوديــت </font> ● · افضل زائر لهذا الاسبوع  · ●<font class="asim11" color="#ff9900"> ضــآيـج </font> ● · افضل زائرة لهذا الاسبوع  · ●<font class="asim11" color="#ff00ff"> آمـي  عـآفـيـتـي </font> ● · مبدع الحائط لهذا الاسبوع · ●<font class="asim11" color="#5CAD9A"> حـطـمـوك يـآقـلـبـي </font> ● · مبدعة الحائط لهذا الاسبوع · ●<font class="asim11" color="#800000"> نـتـالـي </font> ● · افضل تواجد لهذا الاسبوع · ●<font class="asim11" color="#417570"> رهـف بـنـت الـبـاشـا </font>  ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),document.getElementById('tbox').placeholder='\u0639\u0645\u064A \u0639\u0648\u0641 \u0627\u0644\u0633\u0637\u062D \u0648\u062F\u0631\u062F\u0634 \u0648\u064A\u0627\u0646\u0627',$('[data-target=#wall]').attr('title','\u0644\u062A\u0646\u0634\u0631\u0648\u0646 \u0627\u063A\u0627\u0646\u064A'),$(`
-
-
-<div  class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div style="max-width: 390px;"class="modal-dialog" role="document">
-    <div class="modal-content"style="background-color: #FEF5FF;">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 style="text-align: center;font-family: 'DroidArabicKufiRegular',FontAwesome;"class="fl mini  fa fa-heartbeat
-btn btn-info" id="iqr30-com"> قوانين الشات </h4>
-      </div>
-      <div style="padding: 0;" class="asim-iqr32">
-<div dir="rtl" style="text-align: right;padding: 0;margin-right: -20px;" trbidi="on">
-<ul class="main">
-      <li>
-        <div>
-          <span class="asim-iqr30"> قانون ذكر المواقع </span>
-          <br />
-          <span class="asim-iqr3">
-            ممنوع اي شخص يذكر اسم موقع او شات او يروج لهالشي والي يسوي هيج تنسحب لايكاته وينظرب باند 
-         </span> <span style="color: red;">
-      </span>
-        </div>
-      </li>
-       
-       <li>
-        <div>
-          <span class="asim-iqr30">دعاء لسارقين الزوار </span>
-          <br />
-          <span class="asim-iqr3">يارب يموت اعز شخص عندك بالدنيا اذا تبوك زوار من يمنا من غير مكان تبوك وجيبلنا عادي 
-                   </span> <span style="color: red;">
-
-        </div>
-      </li>
-      
-       <li>
-        <div>
-          <span class="asim-iqr30"> قانون التجاوزات </span>
-          <br />
-          <span class="asim-iqr3"> ممنوع اي شخص يجاوز والي يجاوز اخذله سكرين للتجاوز مالته وانطيها للمراقب او الادإره هيه تتصرف  
-		  </span> <span style="color: red;">
-		
-</span>
-        </div>
-      </li>
-      
-       <li>
-        <div>
-          <span class="asim-iqr30">قانون الزحف </span>
-          <br />
-          <span class="asim-iqr3"> ممنوع اي شخص يضايق شخص ثاني بالعام او بالخاص او تنبيهات او جدار والي يسوي هيج ينطرد
-		  		  </span> <span style="color: red;">
-        </span>
-        </div>
-      </li>
-      
-       <li>
-        <div>
-          <span class="asim-iqr30"> قانون الايحاءات</span>
-          <br />
-          <span class="asim-iqr3">ممنوع التحدث بكلام غير اخلاقي حتى ولو كان غير مباشر
-		  		  		  </span> <span style="color: red;">
-
-		  </span>
-        </div>
-      </li>
-	         <li>
-        <div>
-          <span class="asim-iqr30"> قانون السياسة والدين</span>
-          <br />
-          <span class="asim-iqr3">ممنوع التحدث بالسياسه والدين واليحجي يروح بالرجلين
-		  		  		  </span> <span style="color: red;">
-		  </span>
-        </div>
-      </li>
-	         <li>
-        <div>
-          <span class="asim-iqr30"> قانون الجدار</span>
-          <br />
-          <span class="asim-iqr3">تسولف بالجدار عادي تشكر شخص ع منشوره عادي بس مال ادز سمايلات وتتعارك ممنوع اطير لايكاتك
-		  		  		  </span> <span style="color: red;">
-
-</span>
-        </div>
-      </li>
-
-    </div>  
-    
-  
-</div>      </div>
-      <div class="modal-footer">
-        <button style="margin-right: 107px;" type="button" class="btn btn-danger" data-dismiss="modal">إغــلاق نـآفذة  القوانين</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div 
-style="padding: 3px 2px 1px 2px;margin-right:4px;width: 100px;float: right;text-align: center;margin-top: -16px;border-radius: 15px;color: white;background-color: #c23636;"  id="ass" class="fl mini fa fa-heartbeat btn btn-danger" type="button"  data-toggle="modal" data-target="#myModal" >&nbsp; القوانين &nbsp;</div>`).insertBefore('label.fl.nosel.label.pnhead');
-$(".fl.ustat").css("width", "5px")
-
-
-$(".label.label-primary.mini.fl").html(`<span class="s1 fa fa-google-wallet label badgex   label-as-badge label-success"></span>ONline`);
-$(".fa.fa-user-plus").html(`تسجيل`);
-$(".ae.label.label-primary.fa.fa-gear").html(`بروفايل`);
-
