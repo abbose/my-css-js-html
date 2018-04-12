d2ab0d3e7bdd6d53ba3e25b3ab9927fb90f1b10d
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-12 17:13:22 +0300

    Update شات عراق الرومانسية.js

--- شات عراق الرومانسية.js
+++ شات عراق الرومانسية.js
@@ -39,7 +39,7 @@ $(`
 <link rel="stylesheet" href="https://iqr30.github.io/my-css-js-html/شات عراق الرومانسية للجوال.css"><style>
 
 label.label.label-primary.mini.fl {
-    width: 30%!important;
+    width: 33%!important;
     height: 27.5px;
     float: right;
     margin: left;
