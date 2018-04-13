423797cb335eb04ebed02b8840cb97f361063973
iqr30 <36459356+iqr30@users.noreply.github.com>
2018-04-13 18:34:00 +0300

    Update شات عراق الرومانسية.js

--- شات عراق الرومانسية.js
+++ شات عراق الرومانسية.js
@@ -2,9 +2,9 @@ $(` <center><div></div><center>
 <div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · مـوسـيـقـى · ●&nbsp;</div>
 <div class="collapse" id="collapseExample">
 <center><div class="well"><p lass="bg-primary"></i>
-</p>هنا هنا جنا سوا</p>
+</p>راديو</p>
 <audio controls loop="" play="">
-<source src="http://radio.drdchatisite.com:8100/radio.mp3">
+<source src="https://iraqiachats.com/iqr30.mp3">
 </audio>
 </div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
 
